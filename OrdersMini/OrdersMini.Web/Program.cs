48b80449301e6341b93928e6e8fd7359f2a9729b
Bruno Castrillon <brunocastrillon@gmail.com>
2025-08-26 12:19:05 -0400

    - adicionando codigo 'seed' para dar carga inicial no db; - criação do endpoint '/api/auth/login'

--- OrdersMini/OrdersMini.Web/Program.cs
+++ OrdersMini/OrdersMini.Web/Program.cs
@@ -5,8 +5,11 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using OrdersMini.Application.Validations;
 using OrdersMini.Infrastructure;
+using OrdersMini.Infrastructure.Seed;
 using OrdersMini.Web;
 using Serilog;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -83,45 +86,102 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 // === Seed ===
+using (IServiceScope scope = app.Services.CreateScope())
+{
+    AppDbContext db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await AppDbSeeder.SeedAsync(db);
+}
+
+// === Login Fake ===
+var users = new[]
+{
+    new { UsernName = "admin", Password = "123", Role = "Admin" },
+    new { UsernName = "user", Password = "123", Role = "User" }
+};
 
 // === Endpoints ===
 
-
-
-
-
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// => auth
+app.MapPost("/api/auth/login", (string username, string password) =>
 {
-    app.MapOpenApi();
-}
+    var user = users.SingleOrDefault(x => x.UsernName == username && x.Password == password);
+    if (user is null) return Results.Unauthorized();
 
-app.UseHttpsRedirection();
+    Claim[] claims = new[]
+    {
+        new Claim(JwtRegisteredClaimNames.Sub, user.UsernName),
+        new Claim(JwtRegisteredClaimNames.Sub, user.UsernName),
+        new Claim(JwtRegisteredClaimNames.Sub, user.UsernName)
+    };
+
+    SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+    JwtSecurityToken token = new JwtSecurityToken(issuer: jwtSection["Issuer"],
+                                                  audience: jwtSection["Audience"],
+                                                  claims: claims,
+                                                  expires: DateTime.UtcNow.AddHours(8),
+                                                  signingCredentials: credentials);
+
+    return Results.Ok(new
+    {
+        access_token = new JwtSecurityTokenHandler().WriteToken(token)
+    });
+});
+
+// => customers
 
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
-app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
+
+//app.UseHttpsRedirection();
+
+
+//var summaries = new[]
+//{
+//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//};
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 5).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast");
+
+//app.Run();
+
+//internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+//{
+//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+//}
